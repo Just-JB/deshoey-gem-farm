f15979d33f14109c6eb3469b935cc0cee030f75f
matt.sol <matthewcpfeifer@gmail.com>
2022-07-08 14:20:55 -0400

    deshoey assets

--- app/gem-farm/src/App.vue
+++ app/gem-farm/src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="relative h-full min-h-screen bg-rb-mickeypink">
     <TheNavBar />
+    <div class="absolute w-20 top-20 left-64">
+      <img src="/40.png" alt="beer" />
+    </div>
+    <div class="absolute w-20 top-20 right-64">
+      <img src="/40.png" alt="beer" />
+    </div>
     <div class="px-16 mt-2 mx-auto max-w-2xl">
-      <img src="/mickey_degod_banner.png" alt="mickey banner" />
+      <img src="/barrelDAO_logo_black.png" alt="barrelDAO" />
+    </div>
+    <div class="px-16 mt-2 mx-auto max-w-2xl text-center text-3xl">
+      X
+    </div>
+    <div class="px-16 mt-2 mx-auto max-w-2xl">
+      <img src="/DeShoeysbwDEF.png" alt="deshoey" />
     </div>
 
     <div class="p-10">
@@ -15,9 +27,9 @@
         className=" w-52 fixed bottom-0 left-10"
       /> -->
       <img
-        src="/FriesPuddle.png"
-        alt="Fries"
-        className="w-20 fixed bottom-1 right-10"
+        src="/JB.png"
+        alt="JB degods"
+        className="w-64 fixed -bottom-1 right-10"
       />
     </div>
   </div>
